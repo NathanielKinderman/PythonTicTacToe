f47393894ac3615a3ec1cbc017bebb83c41bc1b5
natek <natekinderman16@gmail.com>
2020-04-08 00:56:18 -0500

    Added function for player to place their marker if space availible. And function if players want to replay the game

--- tictac.py
+++ tictac.py
@@ -58,5 +58,17 @@ def full_board_check(board):
             return False
     return True
 
+def player_choice(board):
+    position = 0
+    
+    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
+        position = int(input('Choose your next position: (1-9) '))
+        
+    return position
+
+def replay():
+    
+    return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
+
 
 
