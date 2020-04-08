dc468045b004cbcc1693465e39691226071d74c0
natek <natekinderman16@gmail.com>
2020-04-08 00:36:19 -0500

    Added function to place marker on the board. Based off the board , the position and the will equal the marker. Added function to check if they win, based off board and marker location

--- tictac.py
+++ tictac.py
@@ -1,5 +1,9 @@
+from IPython.display import clear_output
+import random
+
+
 def display_board(board):
-    print('\n'*100)
+    clear_output()
     print(board[1] + '|' + board[2] + '|' + board[3])
     print('-|-|-')
     print(board[4] + '|' + board[5] + '|' + board[6])
@@ -11,16 +15,37 @@ display_board(test_board)
 
 
 def player_input():
+    '''
+    OUTPUT:(Player 1 marker, Player 2 maker)
+    '''
     marker: ''
     #Keep asking player 1 to be x or o
-    while marker != 'X' and marker != 'O':
-        marker = input("Player 1, choose X or O: ")
-    #assign player 2 opposite marker
-    if player1 == 'X':
-        player2 =='O'
+    while not (marker == 'X' or marker == 'O'):
+        marker = input('Player 1: Do you want to be X or O? ').upper()
+
+    if marker == 'X':
+        return ('X', 'O')
     else:
-        player2 == "X"
-    
-    return (player1,player2)
+        return ('O', 'X')
 
 player1_marker,player2_marker = player_input()
+
+def place_marker(board,marker,position):
+    board[position]=marker
+
+def win_check(board,mark):
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or # across the top
+    (board[4] == mark and board[5] == mark and board[6] == mark) or # across the middle
+    (board[1] == mark and board[2] == mark and board[3] == mark) or # across the bottom
+    (board[7] == mark and board[4] == mark and board[1] == mark) or # down the middle
+    (board[8] == mark and board[5] == mark and board[2] == mark) or # down the middle
+    (board[9] == mark and board[6] == mark and board[3] == mark) or # down the right side
+    (board[7] == mark and board[5] == mark and board[3] == mark) or # diagonal
+    (board[9] == mark and board[5] == mark and board[1] == mark)) # diagonal
+
+def choose_first():
+    if random.randint(0,1) == 0:
+        return 'player 2'
+    else:
+        return 'player 1'
+
